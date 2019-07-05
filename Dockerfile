662b0ffbd44b0a23988a27865ce047f0e01569c7
Erik <e.dubovik@artjoker.net>
2019-07-05 17:35:27 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ RUN apt-get update && apt-get install -y golang-go
 WORKDIR /go
 COPY ./server.go ./ 
 
+EXPOSE 8181
 CMD ["/usr/bin/go", "run", "server.go"]
--- server.go
+++ server.go
@@ -8,7 +8,17 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
    fmt.Fprint(resp, m) 
 }
 func main() {
-    msgHandler := msg("Hello from Web Server in Go")
+    msgHandler := msg("<html>" +
+                      "<head>" +
+                      "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" +
+                      "<title>Тег FRAME</title>" +
+                      "</head>"+
+                      "<body>"+
+		      "<img src=\"https://www.sumologic.com/wp-content/uploads/2015/08/DevOps-infinity-loop.png\" alt=\"devops\">"+
+		      "</body>"+
+                      "</frameset>"  +
+                      "</frameset>" +
+                      "</html>")
     fmt.Println("Server is listening...")
-    http.ListenAndServe("localhost:8181", msgHandler)
+    http.ListenAndServe("0.0.0.0:8181", msgHandler)
 }
